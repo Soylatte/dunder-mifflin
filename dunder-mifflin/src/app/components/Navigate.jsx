11f3161dedebbaa233c6860ac08abc720626b415
Soyeon Kim <kksoyeon61@gmail.com>
2024-02-29 15:45:08 +0100

    add via upload

--- dunder-mifflin/src/app/components/Navigate.jsx
+++ dunder-mifflin/src/app/components/Navigate.jsx
@@ -1,11 +1,15 @@
-import React from 'react'
+
 import Link from 'next/link';
+
 export const Navigate = () => {
   return (
     <nav>
-        <header>Trofix</header>
-        <Link href='/About'></Link>
-
+        <header>
+            <Link to='/'>Trofix</Link>
+        <Link to='/재료'>재료</Link>
+        <Link to='/레시피'>레시피</Link>
+        <Link to='/구독하기'>구독</Link>
+        </header>
     </nav>
   )
 }
--- dunder-mifflin/src/app/page.js
+++ dunder-mifflin/src/app/page.js
@@ -1,9 +1,12 @@
+import { ThemeProvider } from "@material-tailwind/react";
 import { Main } from "./components/Main";
+
 export default function Home() {
   return (
-    <main>
-      <h1>Welcome to our website!</h1>
+    <ThemeProvider>
+      <h1>나만의 칵테일을 만들어봐요!</h1>
       <Main />
-    </main>
+    </ThemeProvider>
   );
+
 }
